f90663c393e5b923dfb61f0b384648a8d635c590
Veros Kaplan <VerosK@users.noreply.github.com>
2019-04-23 14:18:20 +0200

    Add public access block to S3 bucket

--- buckets.tf
+++ buckets.tf
@@ -11,6 +11,14 @@ resource "aws_s3_bucket" "backup_bucket" {
   tags = "${var.tags}"
 }
 
+resource "aws_s3_bucket_public_access_block" "backup_block" {
+  bucket = "${aws_s3_bucket.backup_bucket.id}"
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+
 output "bucket_name" {
   value = "${aws_s3_bucket.backup_bucket.arn}"
 }
